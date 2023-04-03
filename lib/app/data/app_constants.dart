00a4fb20bf6ce3b476ecf8c96420f2cee1b88895
savita shukla <savita@gmng.pro>
2023-04-04 02:24:03 +0530

    done

---
+++ lib/app/data/app_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppConstants{
+
+  /// To get Api Key, go to https://beta.openai.com/account/api-keys
+
+  static String apiKey = "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";
+
+}
